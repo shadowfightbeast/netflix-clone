ca844a39c2224e5e83e73a60f90c4ae5ba290c49
Mithilesh Kumar Singh <shadowfightbeast@gmail.com>
2023-08-21 22:56:58 +0530

    project finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+import Row from "./Row";
+import "./App.css";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* navbar  */}
+      {/* banner */}
+
+      <Navbar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOrignals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedynMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt=" Netflix Logo "
+      />
+      <img
+        className="nav__avatar"
+        src="https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png"
+        alt=" avatar Logo "
+      />
+    </div>
+  );
+}
+
+export default Navbar;
